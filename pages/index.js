cfc16debda6675ad8ff67b07c6fca9aeb654023c
m-ruiz21 <38898004+m-ruiz21@users.noreply.github.com>
2021-06-24 06:43:36 -0500

    another one

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
 import Navbar from 'components/navbar/navbar'
 import Body from 'components/body/body'
-import styles from '/styles/Home.module.css'
 
 
 export default function Home() {
